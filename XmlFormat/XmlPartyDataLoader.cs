af201bb8296bb51988337e044bbfc1c61e63bfcf
David Espley <David.Espley@gmail.com>
2012-11-02 14:19:16 +0000

    Added XSD and other tests

--- XmlFormat/XmlPartyDataLoader.cs
+++ XmlFormat/XmlPartyDataLoader.cs
@@ -1,10 +1,17 @@
-﻿namespace XmlFormat
+﻿using System;
+using System.Runtime.Serialization;
+namespace XmlFormat
 {
     public class XmlPartyDataLoader
     {
-       public layWith
-        
-        private XmlParty partyXml = XmlParty.LoadFromFile("XmlPartyInvites.xml");
-        
+       public void PlayWithXml()
+       {
+           var partyXml = EntityBase<XmlPartyParty>.LoadFromFile("XmlPartyInvites.xml");
+           foreach (attendeeType invite  in partyXml.Invite)
+           {
+               Console.WriteLine(invite.id);
+
+           }
+       }
     }
 }
